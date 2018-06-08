602f7a019c48fd8491f050235ffb35740c73e16e
Albert Kim <miktrebla92@gmail.com>
2018-06-07 22:02:43 -0700

    working on spotify

--- liri.js
+++ liri.js
@@ -14,51 +14,54 @@ var input = process.argv[2];
 // console.log(spotify)
 // console.log(twitter)
 
-// var request = require('request');
 
 // // * `my-tweets`
-// // * This will show your last 20 tweets and when they were created at in your terminal/bash window.
-// var params = {
-//     count : 20
-// }
-// var myTweets = twitter.get('statuses/user_timeline', params, results);
-// function results(err, data, response) {
-//     for (var i  = 0; i < params.count; i++){
-//         var tweet = data[i].text;
-//         var created = data[i].created_at
-//         console.log(tweet + ' created at ' + created);
-//     }
-// }
-// //on node command, run function myTweets
-// if (input === 'my-tweets') {
-//     myTweets;
-// }
+
+var params = {
+    count: 20
+}
+var myTweets = twitter.get('statuses/user_timeline', params, results);
+
+function results(err, data, response) {
+    for (var i = 0; i < params.count; i++) {
+        var tweet = data[i].text;
+        var created = data[i].created_at
+        console.log(tweet + ' created at ' + created);
+    }
+}
+//on node command, run function myTweets  to create last 20 tweets
+if (input === 'my-tweets') {
+    myTweets;
+}
 
 // * `spotify-this-song`
-//* This will show the following information about the song in your terminal/bash window
-// * Artist(s) ------------------- 
-// * The song's name
-// * A preview link of the song from Spotify
-// * The album that the song is from
-// * If no song is provided then your program will default to "The Sign" by Ace of Base.
+var spotifyThis = '';
+for (var i = 3; i < process.argv.length; i++) {
+    spotifyThis = spotifyThis + process.argv[i] ;
+}
 
-spotify.search({type : 'track', query: 'tiesto', limit: 2}, function(err,data) {
+
+var spotifyMe = spotify.search({
+    type: 'track',
+    query: spotifyThis,
+    limit: 2
+}, function (err, data) {
     if (err) {
+        // spotifyThis =  'the sign';
+        // spotifyMe;
+        // console.log('We coulodn\'t find what you were looking for. We suggest this song ' + data.tracks.items[0].name + ' by ' + data.tracks.items[0].artist[0].name);
         return console.log('error occurred: ' + err);
-    } 
-    // console.log(data.tracks.items[0].artists[0].name);
-    console.log(data.tracks.items[0].artists[1].name); //name
-    console.log(data.tracks.items[0].name); //song
-    console.log(data.tracks.items[0].preview_url); //preview url of song
-    console.log(data.tracks.items[0]);
-
-
-    
-
-
-  
+    } else {
+        // console.log(data.tracks.items[0].artists[0].name);
+        console.log('Artist: ' + data.tracks.items[0].artists[1].name); //name
+        console.log('Song name: ' + data.tracks.items[0].name); //song
+        console.log('Click this link to preview the track: ' + data.tracks.items[0].preview_url); //preview url of song
+    }
 });
 
+if (input === 'spotify-this-song') {
+    spotifyMe;
+}
 
 
 
@@ -91,7 +94,7 @@ spotify.search({type : 'track', query: 'tiesto', limit: 2}, function(err,data) {
 // * `do-what-it-says`
 
 // * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-     
+
 // * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
 
 // * Feel free to change the text in that document to test out the feature for other commands.
